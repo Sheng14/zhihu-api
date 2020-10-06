852d35040604359c3bb739c53be2017681640177
Sheng14 <1336840941@qq.com>
2020-10-06 19:36:24 +0800

    feat: 自己编写Koa路由中间件

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(async (ctx, next) => {
+app.use(async (ctx) => {
+    if (ctx.url === '/') {
+        ctx.body = '这是首页'
+    } else if (ctx.url === '/users') {
+        if (ctx.method === 'GET') {
+            ctx.body = '获取用户列表'
+        } else if (ctx.method === 'POST') {
+            ctx.body = '注册用户'
+        } else {
+            ctx.status = 405
+        }
+    } else if (ctx.url.match(/\/users\/\w+/)) {
+        const user = ctx.url.match(/\/users\/(\w+)/)[1] // 这里必须加上（），否则只返回true/false！！！，加（）表示捕获组什么的？
+        ctx.body = `当前用户${user}`
+    } else {
+        ctx.status = 404
+    }
+})
+
+
+app.listen(3000)
+
+/*app.use(async (ctx, next) => {
     console.log(1)
     await next()
     console.log(2)
@@ -16,14 +38,23 @@ app.use(async (ctx, next) => {
 
 app.use(async (ctx, next) => {
     console.log(5)
-})
+})*/
 
 // 执行顺序 1 3 5 4 2，所谓洋葱模型，从第一个进去再进去第二个再进去第三个再退到第二个最后到第一个
 // 由于执行了两次（多个favicon），可以采用fetch('/').then((res) => res.text()).then((res) => console.log(res))就只是执行一次
 
-app.listen(3000)
 
 /*利用github测试async await
+
+(
+    async () => {
+        const res = await fetch('//api.github.com/users')
+        console.log(await res.json())
+        const res2 = await fetch('//api.github.com/users/Sheng14')
+        console.log(await res2.json())
+    }
+)()
+
 
 fetch('//api.github.com/users')
     .then((res) => res.json()) //需要先转换成json格式
