6278181c7f60c685933cb20a7739e55ea8e08fb1
Sheng14 <1336840941@qq.com>
2020-10-09 08:59:42 +0800

    feat: Koa自带的错误处理

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,9 +3,13 @@ const db = [{name: '帝国卫队'}]
 
 class UsersCtl {
     find(ctx) {
+       // a.b 模拟500错误
         ctx.body = db
     }
     findById(ctx) {
+        if (ctx.params.id *1 >= db.length) {
+            ctx.throw(412, '查询的id大于数组的长度')
+        } // 模拟412错误
         ctx.body = db[ctx.params.id * 1]
     }
     create(ctx) {
